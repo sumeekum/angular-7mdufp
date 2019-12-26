210188ae793c7bc56ed92156a208b09e77cd1f98
sumeekum <bs.sumeet@gmail.com>
2019-12-26 07:30:45 +0530

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,10 +9,13 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  product;
   constructor( private route: ActivatedRoute,) { }
 
   ngOnInit() {
+  this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
   }
 
 }
